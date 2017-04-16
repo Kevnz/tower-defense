418c51274b5f9ed0c4168776bbcd01c38e281e76
Kevin Isom <kevin.isom@gmail.com>
2017-04-17 09:36:44 +1200

    tidy up the code

--- src/main.js
+++ src/main.js
@@ -1,16 +1,19 @@
-var width = 20 * 16;
-var height = 10 * 16;
-window.game = new Phaser.Game(width, height, Phaser.AUTO);
 require('./plugins/pathfinder');
+
+const width = 20 * 16;
+const height = 10 * 16;
+window.game = new Phaser.Game(width, height, Phaser.AUTO);
+
 game.globals = {
-    TILE_SIZE: 16,
-    WIDTH: 20,
-    HEIGHT: 10,
-    LEVEL: 'level-1',
-    SCORE: 0
+  TILE_SIZE: 16,
+  WIDTH: 20,
+  HEIGHT: 10,
+  LEVEL: 'level-1',
+  SCORE: 0
 };
 
-game.state.add('playtest', require('./states/playtest.js'));
+game.state.add('playtestz', require('./states/playtest.js'));
+game.state.add('playtest', require('./states/placement.js'));
 game.state.add('load', require('./states/load.js'));
 game.state.add('menu', require('./states/menu.js'));
 game.state.add('boot', require('./states/boot.js'));
